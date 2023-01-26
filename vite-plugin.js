972b1cd6f92a4cf8fb1f03a11f039532c73700d3
Thiago <thiago.kaique@stayfilm.com.br>
2023-01-26 10:06:08 -0300

    add parser and unparser

--- vite-plugin.js
+++ vite-plugin.js
@@ -1,63 +1,6 @@
 
-var bodyRegex = /JSXDOM\(([\s\S]*?)\)/g
-var nonEcapsulatedCommas = /,(?![^{}]*})/g
-
-
-var parameterRegex = /(.*?):(.*)/g
-var isOnlyString = /^\s*?"[^"]*"\s*?$/
-var isOnlyNumber = /^\s*?\d*\s*?$/
-
-
-function replaceBody(input,callback)
-{
-    return input.replace(bodyRegex, (match, body) => {
-
-        const [name, properies, children] = body.split(nonEcapsulatedCommas)
-        return "JSXDOM\(" + name + "," + callback(properies, children) + "\)"
-    })
-}
-
-function conditionalyReactive(x)
-{
-    if(isOnlyString.test(x))
-    {
-        return x
-    }
-    
-    if(isOnlyNumber.test(x))
-    {
-        return x
-    }
-    return ` () => ${x}`
-}
-
-
-function replaceParameters(code)
-{
-    return replaceBody(code, (body,children) => {
-
-        var result = body.replace(parameterRegex, (match, variable_name,variable_value) => {
-            if(variable_name.trim() == "model")
-            {
-                return `${variable_name}: [ () => ${variable_value}, _value => ${variable_value} = _value ],`
-            }
-            return variable_name + ":" + conditionalyReactive(variable_value) + ","
-        })
-
-        if(children)
-        {
-            result += "," +
-            children
-                .split(",")
-                .map(conditionalyReactive)
-                .join(",")
-        }
-
-        return result
-    })
-}
-
-module.exports = () => ({
+import { parse } from "./lib/parsecode.js"
+export default () => ({
     name: 'dom-builder',
     config: () => ({
         esbuild: {
@@ -68,9 +11,8 @@ module.exports = () => ({
     }),
     transform(code, id,options) {
         if (id.endsWith('.jsx')) {
-            code = replaceParameters(code)
-            console.log(code)
-            // code = replaceModel(code)
+            code = parse(code);
+            console.log(code);
         }
         return code;
       }
